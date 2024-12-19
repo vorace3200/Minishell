5b37fd5a3bed338f83b65e331859efa3e07899ee
vorace32 <vorace32000@gmail.com>
2024-12-19 16:37:56 +0100

    fixed

--- src/execution/signals/hook_signal.c
+++ src/execution/signals/hook_signal.c
@@ -6,7 +6,7 @@
 /*   By: vorace32 <vorace32000@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 13:05:59 by vorace32          #+#    #+#             */
-/*   Updated: 2024/12/19 16:34:58 by vorace32         ###   ########.fr       */
+/*   Updated: 2024/12/19 16:36:14 by vorace32         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ void	heredoc_signal(int signal)
 {
 	if (signal == SIGINT)
 	{
-		write(STDOUT_FILENO, "\n", 1);
 		g_global_signal = 130;
 	}
 	else if (signal == SIGQUIT)
